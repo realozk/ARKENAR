676ed266d0929d24d35d01731bef520fb058762f
ozk <omarzhrani210@gmail.com>
2026-05-02 13:34:34 +0300

    fix: autoupdte var key

--- gui/src-tauri/src/lib.rs
+++ gui/src-tauri/src/lib.rs
@@ -1024,6 +1024,7 @@ pub fn run() {
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_updater::Builder::new().build())
+        .plugin(tauri_plugin_process::init())
         .invoke_handler(tauri::generate_handler![
             start_scan,
             stop_scan,
